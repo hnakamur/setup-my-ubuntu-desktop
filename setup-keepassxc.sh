b2850c0b348335c37472a21e306191178c90b6c0
Hiroaki Nakamura <hnakamur@gmail.com>
2022-04-24 12:07:50 +0900

    Update setup-keepassxc.sh

--- setup-keepassxc.sh
+++ setup-keepassxc.sh
@@ -4,10 +4,10 @@ set -eu
 repo_url=https://github.com/keepassxreboot/keepassxc
 
 install_fuse() {
-sudo apt install -y fuse libfuse2
-sudo modprobe fuse
-sudo groupadd fuse
-sudo usermod -a -G fuse "$USER"
+  sudo apt install -y fuse libfuse2
+  sudo modprobe fuse
+  getent group fuse || sudo groupadd fuse
+  sudo usermod -a -G fuse "$USER"
 }
 
 install_fuse
@@ -15,22 +15,36 @@ sudo apt install -y curl
 
 version=$(curl -sS -w '%{redirect_url}' -o /dev/null "$repo_url/releases/latest" | sed 's|.*/tag/||')
 
-mkdir -p ~/AppImage
-cd ~/AppImage
-curl -LO ${repo_url}/releases/download/${version}/KeePassXC-${version}-x86_64.AppImage
-chmod +x KeePassXC-${version}-x86_64.AppImage
+app_path="$HOME/AppImage/KeePassXC-${version}-x86_64.AppImage"
+if [ -x "$app_path" ]; then
+  echo KeePassXC latest version $version is already installed.
+else
+  mkdir -p $(dirname "$app_path")
+  app_filename=$(basename "$app_path")
+  curl -Lo "$app_path" ${repo_url}/releases/download/${version}/${app_filename}
+  chmod +x "$app_path"
+  echo Downloaded KeePassXC latest version ($version).
+fi
 
-mkdir -p ~/.icons ~/.local/share/applications
-(cd ~/.icons; curl -LO https://keepassxc.org/images/keepassxc-logo.svg)
+icon_path="$HOME/.icons/keepassxc-logo.svg"
+if [ ! -f "$icon_path" ]; then
+  mkdir -p $(dirname "$icon_path")
+  curl -L -o "$icon_path" https://keepassxc.org/images/keepassxc-logo.svg
+fi
 
-cat > ~/.local/share/applications/keepassxc.desktop <<EOF
+desktop_path=$HOME/.local/share/applications/keepassxc.desktop
+if ! grep -q "^Exec=$HOME/AppImage/KeePassXC-${version}-x86_64.AppImage$" "$desktop_path"; then
+  mkdir -p $(dirname "$desktop_path")
+  cat > "$desktop_path" <<EOF
 #!/usr/bin/env xdg-open
 [Desktop Entry]
-Version=${version}
+Version=1.0
 Type=Application
 Terminal=false
-Exec=/home/hnakamur/AppImage/KeePassXC-${version}-x86_64.AppImage
+Exec=$HOME/AppImage/KeePassXC-${version}-x86_64.AppImage
 Name=KeepassXC
 Comment=Cross-Platform Password Manager
 Icon=keepassxc-logo
 EOF
+  echo Created or updated desktop file for KeePassXC.
+fi

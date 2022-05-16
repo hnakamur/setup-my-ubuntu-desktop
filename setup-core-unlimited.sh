744357d63d17011b12f296bc94978fc1b96b24c1
Hiroaki Nakamura <hnakamur@gmail.com>
2022-05-17 02:15:22 +0900

    Disable apport

--- setup-core-unlimited.sh
+++ setup-core-unlimited.sh
@@ -3,7 +3,13 @@ conf_filename=/etc/security/limits.d/core-unlimited.conf
 
 if [ ! -f "$conf_filename" ]; then
     cat <<'EOF' | sudo tee "$conf_filename" > /dev/null
+root soft core unlimited
+root hard core unlimited
 * soft core unlimited
 * hard core unlimited
 EOF
+fi
+
+if grep -qxF 'enabled=1' /etc/default/apport; then
+    sudo sed -i 's/^enabled=1$/enabled=0/' /etc/default/apport
 fi

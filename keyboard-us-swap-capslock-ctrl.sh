c294f16e993c3b9a62ec508800a1b464d53ea6a1
Hiroaki Nakamura <hnakamur@gmail.com>
2023-01-14 07:24:07 +0900

    Update keyboard-us-swap-capslock-ctrl.sh

--- keyboard-us-swap-capslock-ctrl.sh
+++ keyboard-us-swap-capslock-ctrl.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo sed -i -e '/^XKBMODEL=/s/".*/"pc101"/;/^XKBLAYOUT=/s/".*/"us"/;/^XKBOPTIONS=/s/""/"ctrl:swapcaps"/' /etc/default/keyboard
+sudo sed -i -e '/^XKBMODEL=/s/".*/"pc105"/;/^XKBLAYOUT=/s/".*/"us"/;/^XKBOPTIONS=/s/""/"ctrl:swapcaps"/' /etc/default/keyboard

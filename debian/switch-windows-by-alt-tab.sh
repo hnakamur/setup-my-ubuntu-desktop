7988f507d317ca010cd86391503a795167a39be9
Hiroaki Nakamura <hnakamur@gmail.com>
2023-07-17 09:41:59 +0900

    Add switch-windows-by-alt-tab.sh
    https://superuser.com/a/860001

---
+++ debian/switch-windows-by-alt-tab.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows "['<Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-windows-backward "['<Shift><Alt>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications "['<Super>Tab']"
+dconf write /org/gnome/desktop/wm/keybindings/switch-applications-backward "['<Shift><Super>Tab']"

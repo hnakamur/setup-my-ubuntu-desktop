3aea6c0ae27a126e60db11409c009a33500913f8
Hiroaki Nakamura <hnakamur@gmail.com>
2023-11-27 08:09:21 +0900

    Install ripgrep from GitHub releases

--- setup-ripgrep.sh
+++ setup-ripgrep.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 set -eu
-sudo apt install -y ripgrep
+
+repo_url=https://github.com/BurntSushi/ripgrep
+version=14.0.0
+release=1
+
+install_deb_packages() {
+  for pkg in "$@"; do
+    if [ "$(dpkg-query -f '${Status}' -W $pkg 2>/dev/null)" != 'install ok installed' ]; then
+      echo $pkg
+    fi
+  done | xargs -r sudo apt-get install -y
+}
+
+if [ "$(dpkg-query -f '${Version}' -W nfpm 2>/dev/null)" != "$version-$release" ]; then
+  arch=$(dpkg --print-architecture)
+  deb_filename=ripgrep_${version}-${release}_${arch}.deb
+  deb_url=$repo_url/releases/download/${version}/${deb_filename}
+  install_deb_packages curl
+  curl -sSLo /tmp/$deb_filename $deb_url
+  sudo dpkg -i /tmp/$deb_filename
+fi

1d7c4a4cfd2af2dd8ecb20991d03c97f48bd8c53
Hiroaki Nakamura <hnakamur@gmail.com>
2024-04-29 11:56:43 +0900

    Use iptables to add rules for Incus with Docker

--- setup-incus-nftables.sh
+++ setup-incus-nftables.sh
@@ -4,11 +4,16 @@ set -eu
 #
 # See https://hnakamur.github.io/blog/2022/06/18/adjust-iptables-for-lxd-and-docker/
 #
-# The nftables rules are translated with the following commands:
+# The nftables rules can be translated with the following commands:
 # $ /usr/sbin/iptables-translate -I FORWARD -o incusbr0 -m comment --comment "generated for Incus network incusbr0" -j ACCEPT
 # nft 'insert rule ip filter FORWARD oifname "incusbr0" counter accept comment "generated for Incus network incusbr0"'
 # $ /usr/sbin/iptables-translate -I FORWARD 2 -i incusbr0 -m comment --comment "generated for Incus network incusbr0" -j ACCEPT
 # nft 'insert rule ip filter FORWARD iifname "incusbr0" counter accept comment "generated for Incus network incusbr0"'
+#
+# However running `sudo nft list rulese` print the following warning:
+# # Warning: table ip filter is managed by iptables-nft, do not touch!
+#
+# So It is better to use iptables while Docker uses iptables rather than add rules with nft command.
 
 unitfile=/etc/systemd/system/nftables-for-incus-with-docker.service
 servicename=$(basename "$unitfile")
@@ -22,13 +27,12 @@ After=docker.service
 [Service]
 Type=oneshot
 
-ExecStart=nft insert rule ip filter FORWARD oifname "incusbr0" counter accept comment \"generated for Incus network incusbr0\"
-ExecStart=nft insert rule ip filter FORWARD iifname "incusbr0" counter accept comment \"generated for Incus network incusbr0\"
+ExecStart=/usr/sbin/iptables -I FORWARD -o incusbr0 -m comment --comment "generated for Incus network incusbr0" -j ACCEPT
+ExecStart=/usr/sbin/iptables -I FORWARD 2 -i incusbr0 -m comment --comment "generated for Incus network incusbr0" -j ACCEPT
 
 [Install]
 WantedBy=multi-user.target
 EOF
   sudo systemctl daemon-reload
-  sudo systemctl start "$servicename"
-  sudo systemctl enable "$servicename"
+  sudo systemctl enable --now "$servicename"
 fi

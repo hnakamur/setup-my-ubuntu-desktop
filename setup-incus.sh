bfc34e01a7fe604f9f0c467ba17e8a3d9118deb1
Hiroaki Nakamura <hnakamur@gmail.com>
2024-05-04 20:20:43 +0900

    Use incus from Ubuntu package and create zfs dataset

--- setup-incus.sh
+++ setup-incus.sh
@@ -8,28 +8,38 @@ install_deb_packages() {
   done | xargs -r sudo apt-get install -y
 }
 
-if ! type incus 2>/dev/null; then
-  # https://github.com/zabbly/incus
-
-  if [ ! -f /etc/apt/keyrings/zabbly.asc ]; then
-    install_deb_packages curl
-
-    sudo mkdir -p /etc/apt/keyrings
-    sudo curl -sSo /etc/apt/keyrings/zabbly.asc https://pkgs.zabbly.com/key.asc
+if type zfs >&/dev/null; then
+  dataset=rpool/ROOT/ubuntu/var/lib/incus
+  if ! zfs list "$dataset" >&/dev/null; then
+    sudo zfs create "$dataset"
   fi
+fi
 
-  if [ ! -f /etc/apt/sources.list.d/zabbly-incus-stable.sources ]; then
-    cat <<EOF | sudo tee /etc/apt/sources.list.d/zabbly-incus-stable.sources > /dev/null
+if ! type incus 2>/dev/null; then
+  codename=$(lsb_release -cs)
+  if [ "$codename" != noble ]; then
+    # https://github.com/zabbly/incus
+
+    if [ ! -f /etc/apt/keyrings/zabbly.asc ]; then
+      install_deb_packages curl
+
+      sudo mkdir -p /etc/apt/keyrings
+      sudo curl -sSo /etc/apt/keyrings/zabbly.asc https://pkgs.zabbly.com/key.asc
+    fi
+
+    if [ ! -f /etc/apt/sources.list.d/zabbly-incus-stable.sources ]; then
+      cat <<EOF | sudo tee /etc/apt/sources.list.d/zabbly-incus-stable.sources > /dev/null
 Enabled: yes
 Types: deb
 URIs: https://pkgs.zabbly.com/incus/stable
-Suites: $(lsb_release -cs)
+Suites: $codename
 Components: main
 Architectures: $(dpkg --print-architecture)
 Signed-By: /etc/apt/keyrings/zabbly.asc
 EOF
+    fi
+    sudo apt-get update
   fi
 
-  sudo apt-get update
-  sudo apt-get -y install incus
+  install_deb_packages incus
 fi

99e09a3364ce88853f8557be689c8566ace2ea6c
Hiroaki Nakamura <hnakamur@gmail.com>
2025-06-14 21:20:29 +0900

    Add setup-swiftly.sh

---
+++ setup-swiftly.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# https://www.swift.org/install/linux/
+curl -O https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz && \
+tar zxf swiftly-$(uname -m).tar.gz && \
+./swiftly init --quiet-shell-followup && \
+. "${SWIFTLY_HOME_DIR:-$HOME/.local/share/swiftly}/env.sh" && \
+hash -r

19bb08b83ac4a641ca655a3f6ae720095b8df718
Hiroaki Nakamura <hnakamur@gmail.com>
2025-08-07 13:28:25 +0900

    Add setup-caddy.sh and setup-xcaddy.sh

---
+++ setup-caddy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eu
+repo_url=https://github.com/caddyserver/caddy
+
+install_deb_packages() {
+  for pkg in "$@"; do
+    if [ "$(dpkg-query -f '${Status}' -W $pkg 2>/dev/null)" != 'install ok installed' ]; then
+      echo $pkg
+    fi
+  done | xargs -r sudo apt-get install -y
+}
+
+install_deb_packages curl
+version=$(curl -sS -w '%{redirect_url}' -o /dev/null "$repo_url/releases/latest" | sed 's|.*/tag/||' | sed 's/^v//')
+installed_version=$(caddy --version 2>/dev/null | cut -d ' ' -f 1)
+if [ "$installed_version" != v"$version" ]; then
+  mkdir -p ~/.local/bin
+  arch=$(dpkg --print-architecture)
+  curl -fsSL ${repo_url}/releases/download/v${version}/caddy_${version}_linux_${arch}.tar.gz | tar zx -C ~/.local/bin/ caddy 
+fi

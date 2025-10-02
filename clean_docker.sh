7bd3e780ae177984f8405c5b9e5f846db92e5ccc
Hiroaki Nakamura <hnakamur@gmail.com>
2025-10-02 17:12:34 +0900

    Update clean_docker.sh

--- clean_docker.sh
+++ clean_docker.sh
@@ -2,4 +2,4 @@
 docker system prune --all --force
 docker ps -a -q | xargs -r docker rm -f
 docker images -q moby/buildkit | xargs -r docker rmi
-docker volume rm buildx_buildkit_logunlimited0_state
+docker volume list -q | xargs -r docker volume rm

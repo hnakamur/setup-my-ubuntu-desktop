b114fb9f2509829b75a4541187fd36f8b4fbc497
Hiroaki Nakamura <hnakamur@gmail.com>
2026-03-13 20:36:27 +0900

    Use PlemolJP Console font

--- setup-plemoljp-fonts.sh
+++ setup-plemoljp-fonts.sh
@@ -4,7 +4,7 @@ set -eu
 font_dir=$HOME/.local/share/fonts
 tmp_dir=/tmp
 font_size=14
-font_config_value="PlemolJP $font_size"
+font_config_value="PlemolJP Console $font_size"
 gnome_profile_name=default
 
 install_deb_packages() {
@@ -23,16 +23,18 @@ if [ ! -f "$font_dir/PlemolJP-Regular.ttf" ]; then
   download_url="$repo_url/releases/download/${version}/PlemolJP_${version}.zip"
   local_zip_path="$tmp_dir/PlemolJP_${version}.zip"
   curl -Lo "$local_zip_path" "$download_url"
-  unzip "$local_zip_path" "PlemolJP_${version}/PlemolJP/*.ttf" -d "$tmp_dir"
+  unzip "$local_zip_path" "PlemolJP_${version}/PlemolJPConsole/*.ttf" -d "$tmp_dir"
 
   install -d "$font_dir"
-  install $tmp_dir/PlemolJP_${version}/PlemolJP/*.ttf "$font_dir"
+  install $tmp_dir/PlemolJP_${version}/PlemolJPConsole/*.ttf "$font_dir"
   fc-cache -f "$font_dir"
   rm -rf "$local_zip_path" "$tmp_dir/PlemolJP_${version}"
 fi
 
-profile_id=$(gsettings get org.gnome.Terminal.ProfilesList "$gnome_profile_name" | tr -d "'")
-current_font_config_value=$(gsettings get "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/$profile_id/" font)
-if [ "$current_font_config_value" != "$font_config_value" ]; then
-  gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/$profile_id/" font "$font_config_value"
+if [ -f /usr/bin/gnome-terminal ]; then
+  profile_id=$(gsettings get org.gnome.Terminal.ProfilesList "$gnome_profile_name" | tr -d "'")
+  current_font_config_value=$(gsettings get "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/$profile_id/" font)
+  if [ "$current_font_config_value" != "$font_config_value" ]; then
+    gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/$profile_id/" font "$font_config_value"
+  fi
 fi

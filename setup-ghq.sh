36ab65edda0c1a804ccb9a47d2672ef553031657
Hiroaki Nakamura <hnakamur@gmail.com>
2026-03-13 20:49:17 +0900

    Improve setup-ghq.sh

--- setup-ghq.sh
+++ setup-ghq.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 set -eu
-sudo apt install -y curl unzip
+
+install_deb_packages() {
+  for pkg in "$@"; do
+    if [ "$(dpkg-query -f '${Status}' -W $pkg 2>/dev/null)" != 'install ok installed' ]; then
+      echo $pkg
+    fi
+  done | xargs -r sudo apt-get install -y
+}
+
+install_deb_packages curl unzip
 
 repo_url=https://github.com/x-motemen/ghq
 version=$(curl -sS -w '%{redirect_url}' -o /dev/null "$repo_url/releases/latest" | sed 's|.*/tag/||')
 
-mkdir -p ~/.local/bin
-zip_path=/tmp/ghq_linux_amd64.zip
-curl -L -o ${zip_path}  ${repo_url}/releases/download/${version}/ghq_linux_amd64.zip
-unzip -j ${zip_path} ghq_linux_amd64/ghq -d ~/.local/bin/
+installed_version=$(ghq -version 2>/dev/null | cut -d ' ' -f 3)
+if [ "v$installed_version" != "$version" ]; then
+  bin_dir=$HOME/.local/bin
+  zip_path=/tmp/ghq_linux_amd64.zip
+  mkdir -p "$bin_dir"
+  curl -L -o ${zip_path} ${repo_url}/releases/download/${version}/ghq_linux_amd64.zip
+  unzip -j ${zip_path} ghq_linux_amd64/ghq -d "$bin_dir"
+fi

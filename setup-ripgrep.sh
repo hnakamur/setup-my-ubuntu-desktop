b46ee9f5f66d450861f5f69271c694b4b632b693
Hiroaki Nakamura <hnakamur@gmail.com>
2026-07-17 15:20:24 +0900

    Update setup-ripgrep.sh

--- setup-ripgrep.sh
+++ setup-ripgrep.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-set -eu
+set -eu -o pipefail
 
 repo_url=https://github.com/BurntSushi/ripgrep
-version=14.1.1
-release=1
+version="$(curl -sS -w '%{redirect_url}' -o /dev/null "$repo_url/releases/latest")"
+version="${version##*/}"
 
-install_deb_packages() {
-  for pkg in "$@"; do
-    if [ "$(dpkg-query -f '${Status}' -W $pkg 2>/dev/null)" != 'install ok installed' ]; then
-      echo $pkg
-    fi
-  done | xargs -r sudo apt-get install -y
-}
+bin_dir="${HOME}/.local/bin"
+installed_version="$( ("${bin_dir}/rg" --version 2>/dev/null || :) | head -1)"
+installed_version="${installed_version#* }"
+installed_version="${installed_version%% *}"
+if [ "$installed_version" != "$version" ]; then
+  # install executable
+  arch="$(uname -m)"
+  tarball_url="${repo_url}/releases/download/${version}/ripgrep-${version}-${arch}-unknown-linux-musl.tar.gz"
+  mkdir -p "${bin_dir}"
+  curl -sSL "${tarball_url}" | tar zx -C "${bin_dir}" --strip-components 1 ripgrep-${version}-${arch}-unknown-linux-musl/rg
 
-if [ "$(dpkg-query -f '${Version}' -W nfpm 2>/dev/null)" != "$version-$release" ]; then
-  arch=$(dpkg --print-architecture)
-  deb_filename=ripgrep_${version}-${release}_${arch}.deb
-  deb_url=$repo_url/releases/download/${version}/${deb_filename}
-  install_deb_packages curl
-  curl -sSLo /tmp/$deb_filename $deb_url
-  sudo dpkg -i /tmp/$deb_filename
+  # install bash completions file
+  completions_dir="${HOME}/.local/share/bash-completion/completions"
+  mkdir -p "${completions_dir}"
+  "${bin_dir}/rg" --generate=complete-bash > "${completions_dir}/rg"
 fi
